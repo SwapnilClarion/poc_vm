bf1756dcd6b32894b5e34d627db0d7bb1b2b2700
SwapnilClarion <61285358+SwapnilClarion@users.noreply.github.com>
2021-03-22 16:28:37 +0530

    Initial Commit

---
+++ tests/specs/test1.ts
@@ -0,0 +1,6 @@
+describe("Validate access VM from base machine",()=>{
+    it('should return the title',()=>{
+        console.log(' Title     :    ' +  browser.getTitle());
+        browser.pause(10000);
+    });
+});
